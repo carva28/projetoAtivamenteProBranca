f2cd367c89454408f898f47308323f22d84fafdc
Diogo Carvalho <carvalho_diogo@hotmail.com>
2022-05-22 21:28:46 +0100

    adding new css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Row, Button, Container, Col } from 'react-bootstrap';
+import { Row, Button, Container, Col, Form, FormControl } from 'react-bootstrap';
 import image_main from "./../images/login_character.png";
-import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
+import { auth, registerWithEmailAndPassword, signInWithGoogle } from "./firebase";
 
 
 class NavBar extends React.Component {
@@ -29,30 +29,63 @@ class NavBar extends React.Component {
                 <Col xs={{ order: 1 }} class="" >
                     <h1 class="main_h1_right green_color_text">Ativ@mente</h1>
                     <p class="main_p_right">Nas caixas abaixo, insira os dados que lhe foram dados pela ProBranca.</p>
-                    <input
-                        type="text"
-                        className="login__textBox"
-                        value={this.state.email}
-                        onChange={(e) => this.setState({ "email": e.target.value })}
-                        placeholder="E-mail Address"
-                    />
-                    <input
-                        type="password"
-                        className="login__textBox"
-                        value={this.state.password}
-                        onChange={(e) => this.setState({ "password": e.target.value })}
-                        placeholder="Password"
-                    />
-                    <button
-                        className="login__btn"
-                        onClick={() => signInWithEmailAndPassword(this.state.email, this.state.password)}
-                    >
-                        Login
-                    </button>
-                    <button className="login__btn login__google" onClick={signInWithGoogle}>
-                        Login with Google
-                    </button>
+
+                    <div className="inputs_form_login">
+                        <Col xs={{ order: 1 }} class="inputs_label" >
+                            <Form.Label id="label_p" className="green_color_text">Email address</Form.Label>
+
+                            <Form.Control type="email"
+                                value={this.state.email}
+                                onChange={(e) => this.setState({ "email": e.target.value })}
+                                placeholder="E-mail Address" />
+
+                        </Col>
+                        <Col xs={{ order: 1 }} class="inputs_label" >
+
+                            <Form.Label id="label_p" className="green_color_text">Password </Form.Label>
+
+                            <Form.Control type="password"
+                                value={this.state.password}
+                                onChange={(e) => this.setState({ "password": e.target.value })}
+                                placeholder="Password" />
+
+
+                        </Col>
+                    </div>
+
+                    <div className="conjun_btns_login">
+                        <Col xs={{ order: 1 }} class="_btns_boot" >
+                            <Button
+                                className="login__btn"
+                                onClick={() => registerWithEmailAndPassword(this.state.email, this.state.password)}
+                            >
+                                Login
+                            </Button>
+                        </Col>
+                        <Col xs={{ order: 1 }} class="_btns_boot" >
+                            <Button className="login__btn login__google" onClick={signInWithGoogle}>
+                                Login with Google
+                            </Button>
+                        </Col>
+                    </div>
+                    <Col xs={{ order: 1 }} id="info_help" >
+                        <Row>
+                            <Col xs={{ order: 12 }}  >
+                                <h1>Precisa de ajuda?</h1>
+                                <p>Se precisar de ajuda, clica no botão ao lado direito.</p>
+                            </Col>
+
+                            <Col xs={{ order: 12 }}  >
+                                <Button className="login_help" >
+                                    Login with Google
+                                </Button>
+                            </Col>
+                        </Row>
+
+
+                    </Col>
                 </Col>
+
 
             </Row>
 
