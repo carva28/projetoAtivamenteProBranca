92490a2a6f966b7b9c06fd00ea04304fd34f5e83
Sofia Ribeiro <sofiaribeiro@mbp-de-sofia-2.home>
2022-06-19 17:23:44 +0100

    home done and css parfait

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Card, Button } from "react-bootstrap";
 import Navbar from "../Containers/Navbar";
 import { auth } from "./firebase";
 import emergency from "../images/icons/emergencia.png";
+import consultas from "../images/characters/consultas.svg";
+import medicamentos from "../images/characters/medicamentos.svg";
+import facebook from "../images/icons/facebook.png";
 
 export default class Home extends Component {
   constructor(props) {
@@ -15,23 +18,107 @@ export default class Home extends Component {
     return (
       <div>
         <div className="frame" id="home">
-          <h1 className="green">
-            Bem-vindo <span className="blue">{auth.currentUser.email}</span>
-          </h1>
+          <Row>
+            <Col xs={8}>
+              <h1 className="green">
+                Bem-vindo <span className="blue">{auth.currentUser.email}</span>
+              </h1>
 
-          <p>
-            No painel abaixo veja as suas próximas consultas e os seus contactos
-            mais recentes.
-            <br />
-            Do lado direito pode consultar páginas específicas.
-          </p>
+              <p className="paragraphInfo">
+                No painel abaixo veja as suas próximas consultas e os seus
+                contactos mais recentes.
+                <br />
+                Do lado direito pode consultar páginas específicas.
+              </p>
+            </Col>
 
-          <Button className="btnBorderRed blue" id="emergency">
-            <img src={emergency} alt="ícone de telefone" />
-            Emergência
-          </Button>
+            <Col xs={4} className="btnsAjuda">
+              <Button className="btnInfo blue">i</Button>
 
-          <Button className="btnInfo blue">i</Button>
+              <Button className="btnBorderRed blue" id="emergency">
+                <img src={emergency} alt="ícone de telefone" />
+                Emergência
+              </Button>
+            </Col>
+          </Row>
+
+          <Row className="cards">
+            <Card className="col-6 boxShadow">
+              <h3 className="green">Próximas consultas:</h3>
+
+              <Row>
+                <Col xs={6}>
+                  <p className="bold">29/06 às 14h30</p>
+                  <p>Hospital de Aveiro</p>
+
+                  <p className="bold">03/07 às 10h40</p>
+                  <p>Hospital de Aveiro</p>
+                </Col>
+
+                <Col xs={6}>
+                  <img src={consultas} />
+                </Col>
+              </Row>
+            </Card>
+
+            <Card className="col-6 boxShadow">
+              <h3 className="red">Tome os seguintes medicamentos:</h3>
+
+              <Row>
+                <Col xs={6}>
+                  <p className="bold">29/06 às 14h30</p>
+                  <p>Hospital de Aveiro</p>
+
+                  <p className="bold">03/07 às 10h40</p>
+                  <p>Hospital de Aveiro</p>
+                </Col>
+
+                <Col xs={6} className="alignEnd">
+                  <img src={medicamentos} />
+                </Col>
+              </Row>
+            </Card>
+          </Row>
+
+          <Row>
+            <Col className="outsideSource">
+              <iframe
+                src="https://www.youtube.com/embed/_cVNCuvz8qI?controls=0"
+                title="Canal de Fátima"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                className="boxShadow"
+              ></iframe>
+
+              <Button className="btnFill">
+                <a href="https://youtu.be/_cVNCuvz8qI" target="_blank">
+                  Ver “Fátima” em direto
+                </a>
+              </Button>
+            </Col>
+
+            <Col className="outsideSource">
+              <iframe
+                src="https://www.youtube.com/embed/_cVNCuvz8qI?controls=0"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                className="boxShadow"
+              ></iframe>
+
+              <Button className="btnFill">Ver vídeos da ProBranca</Button>
+            </Col>
+
+            <Col className="outsideSource">
+              <div className="imgSource boxShadow" id="facebook"></div>
+
+              <Button className="btnFill">
+                <a href="fb://facewebmodal/">Consultar Facebook</a>
+              </Button>
+            </Col>
+          </Row>
         </div>
 
         <Navbar />
